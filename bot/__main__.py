0d44b570d5c18ec05c614540d73313e8a2ff9695
Nipfswd <allaj0412@gmail.com>
2026-02-02 13:12:13 +0100

    Actually load the eval cog

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,6 +5,13 @@ from discord.ext.commands import AutoShardedBot, when_mentioned_or
 
 bot = AutoShardedBot(command_prefix=when_mentioned_or(">>>", ">>> "))
 
+# Internal/debug
+bot.load_extension("bot.cogs.logging")
+
+# Owner-only
+bot.load_extension("bot.cogs.eval")
+
+# Commands
 bot.load_extension("bot.cogs.logging")
 bot.load_extension("bot.cogs.bot")
 
